b330b8bcb971137d34edd2c536cc233c84001595
seonho <senho500@naver.com>
2025-04-24 18:23:22 +0900

    Initial commit

---
+++ src/main/java/com/seon/workflow/messagingengine/WorkflowMessagingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.seon.workflow.messagingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowMessagingEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowMessagingEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seon/workflow/messagingengine/WorkflowMessagingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seon.workflow.messagingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowMessagingEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
